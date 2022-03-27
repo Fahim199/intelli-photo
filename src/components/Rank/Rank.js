ccacf8ab515a08af20d1220a79b9b9c064c7b91e
Fahim199 <fahima@mun.ca>
2022-03-27 13:56:08 -0230

    Fixing

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank =({ name, entries})=>{
     return(
         <div>
            <div className='white f3'>
-               {`${name}, your current rank is..`}
+               {`${name}, the number of images you analyzed is...`}
            </div>
            <div className='white f1'>
                {entries}
